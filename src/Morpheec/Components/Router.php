91d791745de6f65ef4451807f7fe04e8b747979c
SynergixeNG <10416482+synergixe@users.noreply.github.com>
2018-09-26 15:26:13 +0100

    Create Router.php

---
+++ src/Morpheec/Components/Router.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2018 Oparand Ltd - Synergixe
+ *
+ * @version v0.0.1
+ *
+ * @author Ifeora Okechukwu (https://twitter.com/isocroft)
+ *
+ * @license MIT
+ *
+ */
+ 
+ 
+ class LaunchRouter {
+ 
+ }
